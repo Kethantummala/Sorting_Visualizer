8a6f1fa1c30ac04343ac33d2b0a201092da9f8ef
Kethantummala <tummala.kethan@gmail.com>
2020-04-02 14:40:35 +0530

    I finally did it! Visualization is working with Bubble sort. so, have to add other algos and maybe make some changes to UI.

--- scripts/sorts.js
+++ scripts/sorts.js
@@ -1,31 +1,53 @@
 //We only have to change background-color and height of the sorting element.
 
+function div_update(div,margin_size,width,height,color)
+{
+    div.style=" margin:0% " + margin_size + "%; background-color:" + color + "; width:" + width + "%; height:" + height + "%;";
+}
+
 function Bubble(div_sizes,divs,cont,n)
 {
     var array_size=n;
     var margin_size=0.1;
+    var factor=Math.floor(n/10);
+    var delay_time=8/factor;        //Decrease numerator to increase speed.
+
+    var c_delay_time=delay_time;
 
     for(var k=0;k<n-1;k++)
     {
         for(var i=0;i<n-k-1;i++)
         {
+
             if(div_sizes[i]>div_sizes[i+1])
             {
-                divs[i].style=" margin:0% " + margin_size + "%; background-color:red; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
-                divs[i+1].style=" margin:0% " + margin_size + "%; background-color:red; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i+1]) + "%;";
+                window.setTimeout(div_update,c_delay_time,divs[i],margin_size,(100/array_size-(2*margin_size)),(div_sizes[i]),"red");
+                c_delay_time+=delay_time;
+                window.setTimeout(div_update,c_delay_time,divs[i+1],margin_size,(100/array_size-(2*margin_size)),(div_sizes[i+1]),"red");
+                c_delay_time+=delay_time;
+
                 var temp=div_sizes[i];
                 div_sizes[i]=div_sizes[i+1];
                 div_sizes[i+1]=temp;
-                divs[i].style=" margin:0% " + margin_size + "%; background-color:red; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
-                divs[i+1].style=" margin:0% " + margin_size + "%; background-color:red; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i+1]) + "%;";
-                divs[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
-                divs[i+1].style=" margin:0% " + margin_size + "%; background-color:green; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i+1]) + "%;";
+
+                window.setTimeout(div_update,c_delay_time,divs[i],margin_size,(100/array_size-(2*margin_size)),(div_sizes[i]),"red");
+                c_delay_time+=delay_time;
+                window.setTimeout(div_update,c_delay_time,divs[i+1],margin_size,(100/array_size-(2*margin_size)),(div_sizes[i+1]),"red");
+                c_delay_time+=delay_time;
+                window.setTimeout(div_update,c_delay_time,divs[i+1],margin_size,(100/array_size-(2*margin_size)),(div_sizes[i+1]),"yellow");
+                c_delay_time+=delay_time;
             }
             else
             {
-                divs[i].style=" margin:0% " + margin_size + "%; background-color:green; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
-                divs[i+1].style=" margin:0% " + margin_size + "%; background-color:green; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i+1]) + "%;";
+                window.setTimeout(div_update,c_delay_time,divs[i],margin_size,(100/array_size-(2*margin_size)),(div_sizes[i]),"yellow");
+                c_delay_time+=delay_time;
             }
+            window.setTimeout(div_update,c_delay_time,divs[i],margin_size,(100/array_size-(2*margin_size)),(div_sizes[i]),"blue");
+            c_delay_time+=delay_time;
         }
+        window.setTimeout(div_update,c_delay_time,divs[i],margin_size,(100/array_size-(2*margin_size)),(div_sizes[i]),"green");
+        c_delay_time+=delay_time;
     }
+    window.setTimeout(div_update,c_delay_time,divs[0],margin_size,(100/array_size-(2*margin_size)),(div_sizes[0]),"green");
+    c_delay_time+=delay_time;
 }
